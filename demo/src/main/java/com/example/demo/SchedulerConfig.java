f51d2223f1c27e7fd70011021a6651a76b193660
Zengdeping <zdp291154601@163.com>
2021-01-02 13:24:26 +0800

    数据库初始化，配置信息修改

--- demo/src/main/java/com/example/demo/SchedulerConfig.java
+++ demo/src/main/java/com/example/demo/SchedulerConfig.java
@@ -24,8 +24,8 @@ public class SchedulerConfig {
     @Bean
     public Properties quartzProperties() throws IOException {
         PropertiesFactoryBean propertiesFactoryBean = new PropertiesFactoryBean();
-        propertiesFactoryBean.setLocation(new ClassPathResource("/quartz.properties"));
         //在quartz.properties中的属性被读取并注入后再初始化对象
+        propertiesFactoryBean.setLocation(new ClassPathResource("/quartz.properties"));
         propertiesFactoryBean.afterPropertiesSet();
         return propertiesFactoryBean.getObject();
     }
